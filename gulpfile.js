dd2e5ac989cbb54fba5bc90c169d1778211da542
bellangerq <bellanger.wall@gmail.com>
2017-08-01 19:20:23 +0200

    Setup gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 'use strict'
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const zip = require('gulp-zip');
 
-gulp.task('sass', function() {
+gulp.task('sass', () =>
   gulp.src('assets/css/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('assets/css'));
-});
+    .pipe(gulp.dest('assets/css'))
+);
 
-gulp.task('default', function() {
-  gulp.watch('src/sass/*.scss', ['sass']);
-});
+gulp.task('default', () =>
+  gulp.watch('src/sass/*.scss', ['sass'])
+);
+
+gulp.task('build', () =>
+	gulp.src([
+    './**',
+    '!*.zip',
+    '!./{node_modules,node_modules/**}'
+    ])
+		.pipe(zip('neige.zip'))
+		.pipe(gulp.dest(''))
+);
