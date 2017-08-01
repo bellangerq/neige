bb0143373ddd6da4d0ff0cc542bc8becb3b6db81
bellangerq <bellanger.wall@gmail.com>
2017-08-01 18:13:33 +0200

    Redefine structure & style header

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
 'use strict'
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const zip = require('gulp-zip');
-const uglify = require('gulp-uglify');
-const pump = require('pump');
 
 gulp.task('sass', function() {
-  gulp.src('src/sass/style.scss')
+  gulp.src('assets/css/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('assets/css'));
 });
@@ -14,18 +11,3 @@ gulp.task('sass', function() {
 gulp.task('default', function() {
   gulp.watch('src/sass/*.scss', ['sass']);
 });
-
-gulp.task('compress', function () {
-  pump([
-        gulp.src('src/js/*.js'),
-        uglify(),
-        gulp.dest('assets/js')
-    ]
-  );
-});
-
-// gulp.task('build', () =>
-// 	gulp.src('*', ['!node_modules'])
-// 		.pipe(zip('archive.zip'))
-// 		.pipe(gulp.dest('dist'))
-// );
