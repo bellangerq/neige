db4fbf3fd6de7ddc9b9c0a335c0de322f083ceb4
bellangerq <bellanger.wall@gmail.com>
2017-08-02 18:09:08 +0200

    Add CSS minify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,20 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const zip = require('gulp-zip');
 const imageOptim = require('gulp-imageoptim');
+const cssnano = require('gulp-cssnano');
 
 gulp.task('sass', () =>
   gulp
     .src('src/sass/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('assets/css'))
+);
+
+gulp.task('minify', () =>
+  gulp
+    .src('assets/css/style.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('assets/css/'))
 );
 
 gulp.task('images', () =>
@@ -29,6 +37,6 @@ gulp.task('compress', () =>
 		.pipe(gulp.dest(''))
 );
 
-gulp.task('build', ['sass', 'images', 'compress'], () =>
+gulp.task('build', ['sass', 'minify', 'images', 'compress'], () =>
   gulp
 );
