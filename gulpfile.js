dc28cd17258d2f77a9440b60471d5c6d3d9d2c91
bellangerq <bellanger.wall@gmail.com>
2017-08-02 12:30:36 +0200

    Fix typo Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,23 @@ const sass = require('gulp-sass');
 const zip = require('gulp-zip');
 
 gulp.task('sass', () =>
-  gulp.src('assets/css/style.scss')
+  gulp
+    .src('assets/css/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('assets/css'))
 );
 
 gulp.task('default', () =>
-  gulp.watch('src/sass/*.scss', ['sass'])
+  gulp
+    .watch('src/sass/*.scss', ['sass'])
 );
 
 gulp.task('build', () =>
-	gulp.src([
-    './**',
-    '!*.zip',
-    '!./{node_modules,node_modules/**}'
+	gulp
+    .src([
+      './**',
+      '!*.zip',
+      '!./{node_modules, node_modules/**}'
     ])
 		.pipe(zip('neige.zip'))
 		.pipe(gulp.dest(''))
